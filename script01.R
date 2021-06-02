7c9a2739edae85d8bbfc60549ae9ea1823a96fba
armando_jr <armandojhunior500@gmail.com>
2021-06-02 00:19:26 -0500

    Script01 actualizado. Tema por completar: Estimadores por razón y regresión

--- script01.R
+++ script01.R
@@ -15,3 +15,34 @@
 # blecimiento es de S/. 2100 y que en una compra se
 # adquieren en promedio 4.4 productos.
 
+# Los datos se muestran en el archivo data que se encuentra
+# ubicada en el mismo directorio
+
+dir()
+
+# -- Pregunta 1 --
+
+#   Seleccione los elementos que conforman la muestra y 
+#   muestre los primeros 2 números seleccionados.
+
+library(readxl)
+datos <- read_xlsx("data01.xlsx")
+colnames(datos) # variables
+dim(datos)      # dimensión del dataframe
+
+# Fase de selección de la muestra
+
+n <- 40            # tamaño de muestra
+N <- dim(datos)[1] # tamaño de población
+f <- n/N           # fracción de muestreo
+
+indices <- sort(sample(N, n, replace = F))
+head(indices,2)    # índices para obtener la muestra
+
+muestra <- datos[indices, ]
+dim(muestra)
+
+# -- Pregunta 2 --
+
+#   ¿Cuál de las 2 variables usaría para mejorar las
+#   estimaciones del monto de compra?
