f6c1b7f67a5d40eaf3f8f30077c8f738db683334
armando_jr <armandojhunior500@gmail.com>
2021-06-02 00:00:18 -0500

    Se creó el ejercicio correspondiente al tema

---
+++ script01.R
@@ -0,0 +1,17 @@
+## MUESTREO ALEATORIO SIMPLE
+
+# Mass es una tienda que aperturó, en estos últimos años,
+# diversos locales en el distrito de San Jerónimo. El
+# gerente desea realizar un análisis de ventas para el
+# mes de febrero, por ello se recopila información de
+# los clientes. Las variables analizadas fueron:
+
+#  Monto: Monto de compra (en soles) del cliente
+#  Ingreso: Ingreso mensual del cliente (en miles de soles)
+#  Número: número de productos comprados
+
+# Se decidió sacar una muestra de 40 registros. Se conoce
+# que el ingreso promedio mensual de un cliente del esta-
+# blecimiento es de S/. 2100 y que en una compra se
+# adquieren en promedio 4.4 productos.
+
